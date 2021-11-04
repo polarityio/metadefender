209313f307a010258f32aeb06e90949ea7608d23
Ed <ed@polarity.io>
2021-11-04 10:32:30 -0400

    Minor template adjustments

--- components/metd-block.js
+++ components/metd-block.js
@@ -1,11 +1,12 @@
 polarity.export = PolarityComponent.extend({
+  details: Ember.computed.alias('block.data.details'),
   actions: {
-       toggleScanner() {
-           this.toggleProperty('isShowingDiv');
-       },
+    toggleScanner() {
+      this.toggleProperty('isShowingDiv');
+    },
 
-       toggleFile() {
-           this.toggleProperty('isShowingFile');
-       }
-   }
+    toggleFile() {
+      this.toggleProperty('isShowingFile');
+    }
+  }
 });
